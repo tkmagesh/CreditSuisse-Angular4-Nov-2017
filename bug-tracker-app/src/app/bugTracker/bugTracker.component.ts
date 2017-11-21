d1cb97b5c98e62f855523f15f8164fd1c2705f44
Magesh <tkmagesh77@gmail.com>
2017-11-21 12:48:19 +0800

    adding, listing, toggling and removing bugs

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IBug } from './models/IBug';
+
 
 @Component({
 	selector : 'bug-tracker',
@@ -6,9 +8,9 @@ import { Component } from '@angular/core';
 		<h1>Bug Tracker</h1>
 		<hr>
 		<section class="stats">
-			<span class="closed">1</span>
+			<span class="closed">{{getClosedCount()}}</span>
 			<span> / </span>
-			<span>2</span>
+			<span>{{bugs.length}}</span>
 		</section>
 		<section class="sort">
 			<label for="">Order By :</label>
@@ -21,24 +23,50 @@ import { Component } from '@angular/core';
 		</section>
 		<section class="edit">
 			<label for="">Bug Name :</label>
-			<input type="text" name="" id="">
-			<input type="button" value="Create New">
+			<input type="text" #txtBugName>
+			<input type="button" value="Create New" (click)="onCreateClick(txtBugName.value)">
 		</section>
 		<section class="list">
 			<ol>
-				<li>
-					<span class="bugname">[This is bug - 1]</span>
-					<div class="datetime">[Created At]</div>
-				</li>
-				<li>
-					<span class="bugname closed">[This is bug - 2]</span>
+				<li *ngFor="let bug of bugs">
+					<span class="bugname" (click)="onBugClick(bug)" [ngClass]="{closed : bug.isClosed}">
+						{{bug | json}}
+					</span>
 					<div class="datetime">[Created At]</div>
 				</li>
 			</ol>
-			<input type="button" value="Remove Closed">
+			<input type="button" value="Remove Closed" (click)="onRemoveClosedClick()">
 		</section>
 	`
 })
 export class BugTrackerComponent{
+	bugs : IBug[] = [];
 
+	onCreateClick(bugName : string){
+		let newBug : IBug = {
+			name : bugName,
+			isClosed : false,
+			createdAt : new Date()
+		}
+		this.bugs.push(newBug);
+	}
+
+	onBugClick(bug : IBug){
+		bug.isClosed = !bug.isClosed;
+	}
+	onRemoveClosedClick(){
+		for(var index = this.bugs.length-1; index >=0; index--){
+			if (this.bugs[index].isClosed)
+				this.bugs.splice(index, 1);
+		}
+	}
+
+	getClosedCount(){
+		let closedCount = 0;
+		for(let index =0, count = this.bugs.length; index < count; index++){
+			if (this.bugs[index].isClosed)
+				++closedCount;
+		}
+		return closedCount;
+	}
 }
